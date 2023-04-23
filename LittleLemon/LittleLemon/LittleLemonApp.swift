54d34fede577b110a6b2249efc33c97bfdf584b6
jessi-z3 <jessi_leeann@yahoo.com>
2023-04-23 10:31:46 -0500

    Homework

---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Jessi Zimmerman on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
